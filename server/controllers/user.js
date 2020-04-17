69a4cebf7c4841098fc265787e241ec9654811df
mohamedisakr <mohamedisakr@gmail.com>
2020-04-17 22:15:19 +0200

    implementing Update user profile

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,3 +11,46 @@ exports.read = (request, response) => {
     response.json(user);
   });
 };
+
+exports.update = (request, response) => {
+  console.log(
+    "UPDATE USER request.user",
+    request.user,
+    "UPDATE DATA",
+    request.body
+  );
+
+  const { name, password } = request.body;
+
+  User.findOne({ _id: request.user._id }, (err, user) => {
+    if (err || !user) {
+      return response.status(400).json({ error: "User not found" });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: "Name is required" });
+    } else {
+      user.name = name;
+    }
+
+    if (password) {
+      if (password.length < 6) {
+        return response
+          .status(400)
+          .json({ error: "Password should be at least 6 characters" });
+      } else {
+        user.password = password;
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log("USER UPDATE ERROR", err);
+        response.status(400).json({ error: "User update failed." });
+      }
+      updatedUser.hashed_passord = undefined;
+      updatedUser.salt = undefined;
+      response.json(updatedUser);
+    });
+  });
+};
